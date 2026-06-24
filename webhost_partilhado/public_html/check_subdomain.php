851cdf43a10483ee6c24cafa826cd4ce7a5c89fb
ice41 <edu.ice7@gmail.com>
2026-06-24 23:26:46 +0100

    Delete webhost_partilhado/public_html/check_subdomain.php

--- webhost_partilhado/public_html/check_subdomain.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<pre>";
-echo "Subdomínio atual: " . $_SERVER['HTTP_HOST'] . "\n";
-echo "IP do servidor: " . gethostbyname($_SERVER['HTTP_HOST']) . "\n";
-echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "\n";
-echo "</pre>";
